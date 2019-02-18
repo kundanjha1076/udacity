2d83409d8687ff849397003331ec78c5178bacf9
Kundan kumar  jha <47710387+kundanjha1076@users.noreply.github.com>
2019-02-18 18:33:23 +0530

    Initial commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+import webbrowser
+import time
+totalbreak=0
+brek_count=3
+print("this program started on"+time.ctime())
+while(totalbreak<brek_count):
+      time.sleep(10)
+      webbrowser.open("kundantuition.tk")
+      
---
+++ findingfile.py
@@ -0,0 +1,4 @@
+import os
+def rename_files():
+    file_list=os.listdir(r"F:\courses\[FreeCoursesOnline.Me] Udacity - Full Stack Web Developer Nanodegree v1.0.0")
+    print(file_list)
